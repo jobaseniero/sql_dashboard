27fefaecb95486c4b37910cf5f144f312b8ad3ba
jobaseniero <job_aseniero@dlsu.edu.ph>
2023-07-20 10:20:33 +0800

    added ticket_priority and chart

--- models/dashboard_main.py
+++ models/dashboard_main.py
@@ -16,6 +16,13 @@ class DashboardMain(models.Model):
           ('deleted', 'Deleted'),
           ], string='Status', default='open')
     
+    ticket_priority = fields.Selection([
+        ('low', 'Low'),
+        ('normal', 'Normal'),
+        ('high', 'High'),
+        ('emergency', 'Emergency'),
+        ], string='Ticket Priority', default='low')
+
     ticket_creator = fields.Char('Ticket Creator')
     team_name = fields.Char('Team Name')
     firstname = fields.Char('First Name')
